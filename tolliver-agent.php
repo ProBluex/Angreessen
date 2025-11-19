ebfc69a2a05982ca8811ceead22d721da3a2c97d
GitHub Action <action@github.com>
2025-11-19 06:21:53 +0000

    Sync from monorepo - Version 3.18.1

--- tolliver-agent.php
+++ tolliver-agent.php
@@ -20,7 +20,7 @@ if (!function_exists('get_file_data')) {
     require_once(ABSPATH . 'wp-includes/functions.php');
 }
 $header = get_file_data(__FILE__, ['Version' => 'Version'], 'plugin');
-define('AGENT_HUB_VERSION', $header['Version'] ?: '3.18.0');
+define('AGENT_HUB_VERSION', $header['Version'] ?: '3.18.1');
 define('AGENT_HUB_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('AGENT_HUB_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('AGENT_HUB_PLUGIN_FILE', __FILE__);
